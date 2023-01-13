3e0b9c5f517c3fad3f8d17c89dd9352d9d8d5233
varunbiluri <varunreddy.billuri@gmail.com>
2023-01-13 14:16:55 +0530

    my first commit

---
+++ Basic Syntax/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]) {
+        int myAge = 22;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+    
+}
---
+++ com.billurivarun/src/One.java
@@ -0,0 +1,9 @@
+public class One{
+    public static void main(String args[]) {
+        int myAge = 21;
+        int herAge = myAge;
+        System.out.println(herAge);
+        
+    }
+
+}
